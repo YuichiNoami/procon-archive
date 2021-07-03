69baea9cf47cc1af8969997de38bc0a7f26d7bf2
yuichin925 <ynoami@2xjack.com>
2021-07-03 13:48:34 +0900

    [AC] abc201 abc201_a

---
+++ atcoder.jp/abc201/abc201_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  vector<int> a(3);
+  for (int i = 0; i < 3; i++)
+  {
+    cin >> a.at(i);
+  }
+  sort(a.begin(), a.end());
+  if (a.at(2) - a.at(1) == a.at(1) - a.at(0))
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+}
